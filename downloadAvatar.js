4cea13f6c0b213f88b94c621cd1f822923df6dde
Isaac Smith <isaac.smitty@gmail.com>
2019-02-11 16:22:46 +0000

    added user agent option

--- downloadAvatar.js
+++ downloadAvatar.js
@@ -5,10 +5,21 @@ var request = require('request');
 console.log('Welcome to the Github Avavtar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+  url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+  headers: {
+    'User-agent': 'request'
+  }
+};
+  request(options, function(err,result, body) {
+    cb(err,body);
+  })
 
 };
+
 
 getRepoContributors('jquery', 'jquery', function(err, result) {
   console.log('Errors::', err);
   console.log('Result:', result);
+
 });
