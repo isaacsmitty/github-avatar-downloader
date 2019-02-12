f798af08548066ed9c55937ac84a2b0bbdebe9f2
Isaac Smith <isaac.smitty@gmail.com>
2019-02-12 00:20:09 +0000

    added response for missing arguments

--- downloadAvatar.js
+++ downloadAvatar.js
@@ -1,29 +1,40 @@
 var request = require('request');
 var fs = require('fs');
+
 var token = require('./secrets.js')
-//var api = process.argv.slice(2);
+
+var owner = process.argv[2];
+var name = process.argv[3];
 
 console.log('Welcome to the Github Avavtar Downloader!');
 
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-    headers: {
-      'User-agent': 'request', 'Authorization': `token ${token.GITHUB_TOKEN}`
-    }
-  };
+  if (owner === undefined || name === undefined) {
 
-  request(options, function(err, result, body) {
-    var json = JSON.parse(body)
-    var contributor = json[0];
-    var avatar = contributor['avatar_url'];
-    json.forEach(function(contributor) {
-      avatar = contributor['avatar_url']
-      downloadImageByURL(avatar, `./avatars/${contributor.login}.jpg`);
-    cb(err, avatar)
-    })
+    console.log('Please enter valid repo owner and name.');
 
-  });
+  } else {
+
+    var options = {
+      url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+      headers: {
+        'User-agent': 'request', 'Authorization': `token ${token.GITHUB_TOKEN}`
+      }
+    };
+
+    request(options, function(err, result, body) {
+      var json = JSON.parse(body)
+      var contributor = json[0];
+      var avatar = contributor['avatar_url'];
+      json.forEach(function(contributor) {
+        avatar = contributor['avatar_url']
+        downloadImageByURL(avatar, `./avatars/${contributor.login}.jpg`);
+      cb(err, avatar)
+      })
+
+    });
+  }
 }
 
 function downloadImageByURL(url, filePath) {
@@ -43,7 +54,7 @@ function downloadImageByURL(url, filePath) {
 
 }
 
-getRepoContributors('jquery', 'jquery', function(err, result) {
+getRepoContributors(owner, name, function(err, result) {
 
   console.log(result);
 
